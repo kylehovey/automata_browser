5f6b53d74ddf53227fc073f73850bc3861c89fc4
khoveyc@gmail.com <khoveyc@gmail.com>
2020-05-19 19:30:44 -0600

    Added way to set rule number

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Terrarium from './components/terra/terrarium';
+import { nameForRuleNumber } from './lib/ca.js';
 
-const App = () => (
-  <div>
-    <h1>Entropy Exploration</h1>
-    <Terrarium
-      width={100}
-      height={100}
-      cellSize={5}
-      ruleNumber={6152}
-    />
-  </div>
-);
+const App = () => {
+  const [ ruleNumber, setRuleNumber ] = useState(6152);
+
+  const methods = {
+    onRuleNumberChange({ target }) {
+      setRuleNumber(target.value);
+    },
+  };
+
+  return (
+    <div>
+      <h1>Entropy Exploration</h1>
+      <hr />
+      <input
+        type="number"
+        value={ruleNumber}
+        onChange={methods.onRuleNumberChange}
+      />
+      <span>{nameForRuleNumber(ruleNumber)}</span>
+      <Terrarium
+        width={100}
+        height={100}
+        cellSize={5}
+        ruleNumber={ruleNumber}
+      />
+    </div>
+  );
+};
 
 export default App;
